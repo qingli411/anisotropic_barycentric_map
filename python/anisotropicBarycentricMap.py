c31fc01690af5c948b988d2a41621053d31b37f2
Qing Li <lqingpku@gmail.com>
2022-12-06 20:17:09 +0800

    Working on python support

---
+++ python/anisotropicBarycentricMap.py
@@ -0,0 +1,13 @@
+
+class ReynoldsStress(object):
+    """ ReynoldsStress object
+    """
+
+    def init(self, uu, vv, ww, uv, uw, vw, ):
+        """Initialize Reynolds stress.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
